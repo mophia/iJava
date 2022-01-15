5f19af8760dd30c1819f654aa41944d3696f37e7
jun <plus@outlook.at>
2022-01-15 16:01:58 +0800

    init

---
+++ niceJava/src/com/sooia/day04/demo01/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.sooia.day04.demo01;
+// 单行注释
+/* 单行注释 */
+/*
+* 多行
+* 注释
+* */
+
+// public class 后面代表定义一个类的名称。类是Java中所有源代码的基本组织单位
+public class HelloWorld {
+    // 第二行的内容是万年不变的固定写法，代表main方法，代表程序执行的起点
+    public static void main(String[] args) {
+        // 第三行代表打印输出语句，屏幕显示
+        System.out.println("Hello, baby!");
+    }
+}
---
+++ niceJava/src/com/sooia/day04/demo02/Const.java
@@ -0,0 +1,5 @@
+package com.sooia.day04.demo02;
+
+public class Const {
+    
+}
